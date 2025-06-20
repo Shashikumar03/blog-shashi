4dbb9b7d7164c6161a800c4e4688f75886615c66
ShashiCard91 <shashi.kushwaha@card91.io>
2025-06-21 00:19:19 +0530

    env

--- src/main/java/com/example/blogshashi/config/SecurityConfig.java
+++ src/main/java/com/example/blogshashi/config/SecurityConfig.java
@@ -57,6 +57,7 @@ public class SecurityConfig {
                 .requestMatchers(HttpMethod.GET).permitAll()
                 .requestMatchers(PUBLIC_URLS).permitAll()
                 .requestMatchers("/auth/login").permitAll()
+                .requestMatchers("api/v1/auth/login").permitAll()
                 .anyRequest()
 //                .authenticated()
                 .permitAll()
